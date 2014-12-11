c560330107780140547d0063d4de53d4f25d37f0
Chih-Yung Liang <liangcy@cs.nctu.edu.tw>
2014-12-11 11:33:18 +0800

    完成

--- PP-hw4/cuda_wave.cu
+++ PP-hw4/cuda_wave.cu
@@ -7,96 +7,62 @@
 #include <device_launch_parameters.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include <time.h>
 
 #define MAXPOINTS 1000000
 #define MAXSTEPS 1000000
 #define MINPOINTS 20
 #define PI 3.14159265
 
-void check_param(void);
-void init_line(void);
-void update(void);
-void printfinal(void);
-
-int nsteps,                 	/* number of time steps */
-tpoints, 	     		/* total points along string */
-rcode;                  	/* generic return code */
-float  values[MAXPOINTS + 2], 	/* values at time t */
-oldval[MAXPOINTS + 2], 	/* values at time (t-dt) */
-newval[MAXPOINTS + 2]; 	/* values at time (t+dt) */
-
 #define cudaFn(fn,...) { \
 	cudaError_t __cudaStatus = fn(__VA_ARGS__); \
-	if (__cudaStatus != cudaSucccess) { \
+	if (__cudaStatus != cudaSuccess) { \
 		fprintf(stderr, "Failed when calling cuda function \"%s\"!\n%s\n", #fn, cudaGetErrorString(__cudaStatus)); \
 		exit(1); \
 	} \
 }
-#define kernelFn(fn,...) { \
-	fn(__VA_ARGS__); \
+#define kernelFn(fn,block_count,thread_count,...) { \
+	fn<<<block_count, thread_count>>>(__VA_ARGS__); \
 	cudaError_t __cudaStatus = cudaGetLastError(); \
 	if (__cudaStatus != cudaSuccess) { \
-		fprintf(stderr, "%s launch failed: %s\n", #fn, cudaGetErrorString(__cudaStatus); \
+		fprintf(stderr, "%s launch failed: %s\n", #fn, cudaGetErrorString(__cudaStatus)); \
 		exit(1); \
 	} \
 }
 
+float result[MAXPOINTS + 2];
+
 /**********************************************************************
 *	Checks input values from parameters
 *********************************************************************/
-void check_param(void) {
+__host__ void check_param(int *tpoints, int *nsteps) {
 	char tchar[20];
 
 	/* check number of points, number of iterations */
-	while ((tpoints < MINPOINTS) || (tpoints > MAXPOINTS)) {
+	while ((*tpoints < MINPOINTS) || (*tpoints > MAXPOINTS)) {
 		printf("Enter number of points along vibrating string [%d-%d]: "
 			   , MINPOINTS, MAXPOINTS);
 		scanf("%s", tchar);
-		tpoints = atoi(tchar);
-		if ((tpoints < MINPOINTS) || (tpoints > MAXPOINTS))
+		*tpoints = atoi(tchar);
+		if ((*tpoints < MINPOINTS) || (*tpoints > MAXPOINTS))
 			printf("Invalid. Please enter value between %d and %d\n",
 			MINPOINTS, MAXPOINTS);
 	}
-	while ((nsteps < 1) || (nsteps > MAXSTEPS)) {
+	while ((*nsteps < 1) || (*nsteps > MAXSTEPS)) {
 		printf("Enter number of time steps [1-%d]: ", MAXSTEPS);
 		scanf("%s", tchar);
-		nsteps = atoi(tchar);
-		if ((nsteps < 1) || (nsteps > MAXSTEPS))
+		*nsteps = atoi(tchar);
+		if ((*nsteps < 1) || (*nsteps > MAXSTEPS))
 			printf("Invalid. Please enter value between 1 and %d\n", MAXSTEPS);
 	}
 
-	printf("Using points = %d, steps = %d\n", tpoints, nsteps);
+	printf("Using points = %d, steps = %d\n", *tpoints, *nsteps);
 
-}
-
-/**********************************************************************
-*     Initialize points on line
-*********************************************************************/
-void init_line(void) {
-	int i, j;
-	float x, fac, k, tmp;
-
-	/* Calculate initial values based on sine curve */
-	fac = 2.0 * PI;
-	k = 0.0;
-	tmp = tpoints - 1;
-	for (j = 1; j <= tpoints; j++) {
-		x = k / tmp;
-		values[j] = sin(fac * x);
-		k = k + 1.0;
-	}
-
-	/* Initialize old values array */
-	for (i = 1; i <= tpoints; i++)
-		oldval[i] = values[i];
 }
 
 /**********************************************************************
 *      Calculate new values using wave equation
 *********************************************************************/
-void do_math(int i) {
+__device__ float do_math(float values, float oldval) {
 	float dtime, c, dx, tau, sqtau;
 
 	dtime = 0.3;
@@ -104,38 +70,58 @@ void do_math(int i) {
 	dx = 1.0;
 	tau = (c * dtime / dx);
 	sqtau = tau * tau;
-	newval[i] = (2.0 * values[i]) - oldval[i] + (sqtau *  (-2.0)*values[i]);
+	return (2.0 * values) - oldval + (sqtau *  (-2.0)*values);
 }
 
-/**********************************************************************
-*     Update all values along line a specified number of times
-*********************************************************************/
-void update() {
-	int i, j;
+__global__ void update(float *values, int tpoints, int nsteps) {
+	/**********************************************************************
+	*     Initialize points on line
+	*********************************************************************/
+	int idx = blockIdx.x * blockDim.x + threadIdx.x + 1;
+	float x, fac;
+	float nowval,	/* values at time t */ 
+		oldval,		/* values at time (t-dt) */
+		newval;		/* values at time (t+dt) */
+
+	/* Calculate initial values based on sine curve */
+	fac = 2.0 * PI;
+	x = (-1.0 + idx) / (float) (tpoints - 1);
+
+	nowval = sin(fac * x);
+
+	/* Initialize old values array */
+	oldval = nowval;
+
+
+
+
+	/**********************************************************************
+	*     Update all values along line a specified number of times
+	*********************************************************************/
+	int i;
 
 	/* Update values for each time step */
-	for (i = 1; i <= nsteps; i++) {
+#pragma unroll 1000
+	for (i = 0; i < nsteps; i++) {
 		/* Update points along line for this time step */
-		for (j = 1; j <= tpoints; j++) {
-			/* global endpoints */
-			if ((j == 1) || (j == tpoints))
-				newval[j] = 0.0;
-			else
-				do_math(j);
-		}
+		newval = do_math(nowval, oldval);
 
 		/* Update old values with new values */
-		for (j = 1; j <= tpoints; j++) {
-			oldval[j] = values[j];
-			values[j] = newval[j];
-		}
+		oldval = nowval;
+		nowval = newval;
 	}
+
+	/* global endpoints */
+	if ((idx == 1) || (idx == tpoints))
+		nowval = 0.0;
+
+	values[idx] = nowval;
 }
 
 /**********************************************************************
 *     Print final results
 *********************************************************************/
-void printfinal() {
+__host__ void printfinal(float values[], int tpoints) {
 	int i;
 
 	for (i = 1; i <= tpoints; i++) {
@@ -148,17 +134,33 @@ void printfinal() {
 /**********************************************************************
 *	Main program
 *********************************************************************/
-int main(int argc, char *argv[]) {
+__host__ int main(int argc, char *argv[]) {
+	int nsteps,                 	/* number of time steps */
+		tpoints; 	     		/* total points along string */
+	float *values;
+
+	if (argc != 3) {
+		printf("Usage: %s <number_of_points> <number_of_time_steps>\n", argv[0]);
+		return 1;
+	}
 	sscanf(argv[1], "%d", &tpoints);
 	sscanf(argv[2], "%d", &nsteps);
-	check_param();
+	check_param(&tpoints, &nsteps);
+
+	int blockCount = tpoints / 1024 + 1;
+	int arySize = (blockCount * 1024 + 1) * sizeof(float);
+	cudaFn(cudaMalloc, &values, arySize);
+
 	printf("Initializing points on the line...\n");
-	init_line();
 	printf("Updating all points for all time steps...\n");
-	update();
+	kernelFn(update, blockCount, 1024, values, tpoints, nsteps);
+
 	printf("Printing final results...\n");
-	printfinal();
+	cudaFn(cudaMemcpy, result, values, arySize, cudaMemcpyDeviceToHost);
+	printfinal(result, tpoints);
 	printf("\nDone.\n\n");
 
+	cudaFn(cudaFree, values);
+	cudaFn(cudaDeviceReset);
 	return 0;
 }
